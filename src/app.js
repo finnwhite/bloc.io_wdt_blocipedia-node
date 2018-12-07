9ed4b9d355e0b433cd5aaa69d7295641d5a054d9
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-12-06 20:55:35 -0800

    Setup delpoy to production

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require( "express" );
 const app = express();
 
+require( "./config/app_config.js" ).init( app, express );
 require( "./config/route_config.js" ).init( app );
 
 module.exports = app;
