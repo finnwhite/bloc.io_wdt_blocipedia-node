8fb432a562a6e2c636a109f1427ba6f59b8eb4bf
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-12-10 20:57:50 -0800

    Install dependencies
    - Installed body-parser middleware, faker.js for dummy seed data
    - Configured Sequelize, installed Sequelize CLI to run on production

--- src/config/app_config.js
+++ src/config/app_config.js
@@ -1,14 +1,19 @@
 require( "dotenv" ).config();
 const path = require( "path" );
+const viewsDir = path.join( __dirname, "..", "views" );
+const assetsDir = path.join( __dirname, "..", "assets" );
+const bodyParser = require( "body-parser" );
 
 module.exports = {
 
   init( app, express ) {
 
-    app.set( "views", path.join( __dirname, "..", "views" ) );
+    app.set( "views", viewsDir );
     app.set( "view engine", "ejs" );
 
-    app.use( express.static( path.join( __dirname, "..", "assets" ) ) );
+    app.use( bodyParser.urlencoded( { extended: true } ) );
+
+    app.use( express.static( assetsDir ) );
 
   },
 
